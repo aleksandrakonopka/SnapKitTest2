bfd6c0c0299a82bf30deb31acab4317e605d6bfc
Aleksandra Konopka <aleksandrakonopka424@gmail.com>
2019-12-27 19:57:07 +0100

    Initial Commit

---
+++ SnapKitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapKitTest2
+//
+//  Created by Aleksandra on 27/12/2019.
+//  Copyright © 2019 Aleksandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
