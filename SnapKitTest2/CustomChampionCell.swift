1b24c3758c58d95dff5f9eef23eeba390d491df5
Aleksandra Konopka <aleksandrakonopka424@gmail.com>
2019-12-28 22:57:21 +0100

    stack views added

--- SnapKitTest2/CustomChampionCell.swift
+++ SnapKitTest2/CustomChampionCell.swift
@@ -17,36 +17,55 @@ class CustomChampionCell : UITableViewCell{
     var championTypeFirst : UIImageView?
     var championTypeSecond : UIImageView?
     
+    var linesStackView : UIStackView!
+    var linesAndNameStackView : UIStackView!
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configure()
     }
-    
     func configure(){
         championNameLabel = UILabel(frame: .zero)
-        self.contentView.addSubview(championNameLabel)
-        championNameLabel.snp.makeConstraints {
-            $0.left.equalToSuperview().offset(2)
-            $0.bottom.equalToSuperview().offset(-2)
-        }
-        
         championLineFirst = UIImageView()
-        self.contentView.addSubview(championLineFirst)
-        championLineFirst.snp.makeConstraints{
-            $0.left.equalToSuperview().offset(5)
-            $0.size.equalTo(CGSize(width: 40, height: 40))
-            $0.top.equalToSuperview().offset(10)
-        }
-        
         championLineSecond = UIImageView()
-        self.contentView.addSubview(championLineSecond)
-        championLineSecond.snp.makeConstraints{
-            $0.left.equalTo(championLineFirst).offset(50)
-            $0.size.equalTo(CGSize(width: 40, height: 40))
-            $0.top.equalToSuperview().offset(10)
-        }
         
+        setUpLeftStackViews()
+        
+        self.contentView.addSubview(linesAndNameStackView)
+        
+        setUpLeftConstraints()
+    }
+    
+    func setUpLeftStackViews(){
+        setUpLinesStackView()
+        setupLinesAndNameStackView()
+    }
+    func setUpLinesStackView(){
+        linesStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 100, height: 90))//zamiast frame uzyc constrainty
+        linesStackView.axis = .horizontal
+        linesStackView.addArrangedSubview(championLineFirst)
+        linesStackView.addArrangedSubview(championLineSecond)
+        linesStackView.spacing = 20
+    }
+    func setupLinesAndNameStackView(){
+        linesAndNameStackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 100, height: 80))
+        linesAndNameStackView.axis = .vertical
+        linesAndNameStackView.addArrangedSubview(linesStackView)
+        linesAndNameStackView.addArrangedSubview(championNameLabel)
+        linesAndNameStackView.spacing = 20
+    }
+    func setUpLeftConstraints(){
+        linesAndNameStackView.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(10)
+            $0.left.equalToSuperview().offset(10)
+        }
+        championLineFirst.snp.makeConstraints{
+            $0.size.equalTo(CGSize(width: 40, height: 40))
+        }
+        championLineSecond.snp.makeConstraints{
+              $0.size.equalTo(CGSize(width: 40, height: 40))
+        }
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
