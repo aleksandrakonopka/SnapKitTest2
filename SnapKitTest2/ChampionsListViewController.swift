5bed138188b75deb8ccbc0ef61d9b053b8750f22
Aleksandra Konopka <aleksandrakonopka424@gmail.com>
2019-12-28 18:17:10 +0100

    split code into functions

--- SnapKitTest2/ChampionsListViewController.swift
+++ SnapKitTest2/ChampionsListViewController.swift
@@ -13,35 +13,32 @@ class ChampionsListViewController: UIViewController {
      var tableView : UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        createTable()
+        view.backgroundColor = .yellow
+        title = "League of Legends Champions"
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setupTableViewConstraints()
+    }
+    func setupTableViewConstraints(){
+        tableView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(0)
+            make.left.equalTo(view).offset(0)
+            make.bottom.equalTo(view).offset(0)
+            make.right.equalTo(view).offset(0)
+        }//do view Will appear
+    }
+    func createTable(){
         tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(CustomChampionCell.self, forCellReuseIdentifier: "ChampionCell")
         
-        view.backgroundColor = .yellow
-        let box = UIView()
-        box.backgroundColor = .green
-        //view.addSubview(box)
         view.addSubview(tableView)
         tableView.reloadData()
-        tableView.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(view).offset(20)
-            make.left.equalTo(view).offset(20)
-            make.bottom.equalTo(view).offset(-20)
-            make.right.equalTo(view).offset(-20)
-        }
-        title = "League of Legends Champions"
-//        box.snp.makeConstraints { (make) -> Void in
-//            make.top.equalTo(view).offset(20)
-//            make.left.equalTo(view).offset(20)
-//            make.bottom.equalTo(view).offset(-20)
-//            make.right.equalTo(view).offset(-20)
-//        }
     }
-
-
 }
-
 extension ChampionsListViewController : UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 30
